1ab65edb7d44d59cb46ae6cd1edc9a1f65fe3df8
skc3766 <skc3766@gmail.com>
2024-12-03 12:44:01 +0530

    setting default port

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
+import uvicorn
 from fastapi import FastAPI
+import os
 from app.routes.student_routes import router as student_router
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if $PORT is not set
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
 
 app.include_router(student_router, prefix="/api")
